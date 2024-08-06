2404f9c44034b116e00f48cb35a3753be49c2a48
butterfly026 <butterfly026@github.com>
2024-08-06 11:25:04 -0500

    update for translation

--- lib/view/subscription/subscription_details_page.dart
+++ lib/view/subscription/subscription_details_page.dart
@@ -34,148 +34,156 @@ class _SubscriptionDetailsPageState extends State<SubscriptionDetailsPage> {
       body: SingleChildScrollView(
         physics: physicsCommon,
         child: Consumer<SubscriptionService>(
-          builder: (context, provider, child) => Container(
-            padding:
-                EdgeInsets.symmetric(horizontal: screenPadding, vertical: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CommonHelper().paragraphCommon(
-                    asProvider.getString(
-                        'Note: Pending connect will be added to available connect only if the payment status is completed'),
-                    TextAlign.left),
-                Container(
-                  decoration: const BoxDecoration(shape: BoxShape.circle),
-                ),
+          builder: (context, provider, child) => Consumer<SubscriptionService>(
+            builder: (context, provider, child) => Container(
+              padding:
+                  EdgeInsets.symmetric(horizontal: screenPadding, vertical: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CommonHelper().paragraphCommon(
+                      asProvider.getString(
+                          'Note: Pending connect will be added to available connect only if the payment status is completed'),
+                      TextAlign.left),
+                  Container(
+                    decoration: const BoxDecoration(shape: BoxShape.circle),
+                  ),
 
-                sizedBoxCustom(25),
-                //Title,  type
-                //===================>
-                Container(
-                  margin: const EdgeInsets.only(bottom: 25),
-                  width: double.infinity,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(9)),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CommonHelper().titleCommon(provider.subsData.type +
-                            ' ' +
-                            asProvider.getString('subscription')),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        //Service row
-
-                        Text(
-                          asProvider.getString("Type") +
-                              ": ${provider.subsData.type}",
-                          style: TextStyle(
-                            color: cc.greyFour,
-                            fontSize: 14,
-                            height: 1.4,
+                  sizedBoxCustom(25),
+                  //Title,  type
+                  //===================>
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 25),
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(9)),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CommonHelper().titleCommon(provider.subsData.type +
+                              ' ' +
+                              asProvider.getString('subscription')),
+                          const SizedBox(
+                            height: 10,
                           ),
-                        ),
+                          //Service row
 
-                        sizedBoxCustom(10),
-                        Text(
-                          asProvider.getString('Expire date') +
-                              ': ' +
-                              formatDate(provider.subsData.expireDate),
-                          style: TextStyle(
-                            color: cc.primaryColor,
-                            fontSize: 14,
-                            height: 1.4,
+                          Text(
+                            asProvider.getString("Type") +
+                                ": ${provider.subsData.type}",
+                            style: TextStyle(
+                              color: cc.greyFour,
+                              fontSize: 14,
+                              height: 1.4,
+                            ),
                           ),
-                        ),
-                      ]),
-                ),
 
-                //Connect details
-                //================>
-                Container(
-                  margin: const EdgeInsets.only(bottom: 25),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(9)),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CommonHelper().titleCommon(
-                            asProvider.getString('Connect details')),
-                        const SizedBox(
-                          height: 25,
-                        ),
-                        //Service row
+                          sizedBoxCustom(10),
+                          Text(
+                            asProvider.getString('Expire date') +
+                                ': ' +
+                                formatDate(provider.subsData.expireDate),
+                            style: TextStyle(
+                              color: cc.primaryColor,
+                              fontSize: 14,
+                              height: 1.4,
+                            ),
+                          ),
+                        ]),
+                  ),
 
-                        Container(
-                          child: PaymentHelper().bRow(
-                              'null',
-                              'Available connect',
-                              '${provider.subsData.connect}'),
-                        ),
+                  //Connect details
+                  //================>
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 25),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(9)),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CommonHelper().titleCommon(
+                              asProvider.getString('Connect details')),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          //Service row
 
-                        Container(
-                          child: PaymentHelper().bRow('null', 'Pending connect',
-                              '${provider.subsData.initialConnect}',
-                              lastBorder: false),
-                        ),
-                      ]),
-                ),
+                          Container(
+                            child: PaymentHelper().bRow(
+                                'null',
+                                'Available connect',
+                                '${provider.subsData.connect}'),
+                          ),
 
-                //Payment details
-                //================>
-                Container(
-                  margin: const EdgeInsets.only(bottom: 25),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(9)),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CommonHelper().titleCommon(
-                            asProvider.getString('Payment details')),
-                        const SizedBox(
-                          height: 25,
-                        ),
-                        //Service row
+                          Container(
+                            child: PaymentHelper().bRow(
+                                'null',
+                                'Pending connect',
+                                '${provider.subsData.initialConnect}',
+                                lastBorder: false),
+                          ),
+                        ]),
+                  ),
 
-                        Container(
-                          child: PaymentHelper().bRow('null', 'Payment gateway',
-                              '${removeUnderscore(provider.subsData.paymentGateway ?? "---")}'),
-                        ),
+                  //Payment details
+                  //================>
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 25),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(9)),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CommonHelper().titleCommon(
+                              asProvider.getString('Payment details')),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          //Service row
 
-                        Container(
-                          child: PaymentHelper().bRow('null', 'Payment status',
-                              '${provider.subsData.paymentStatus}',
-                              lastBorder: false),
-                        ),
-                      ]),
-                ),
+                          Container(
+                            child: PaymentHelper().bRow(
+                                'null',
+                                'Payment gateway',
+                                '${removeUnderscore(provider.subsData.paymentGateway ?? "---")}'),
+                          ),
 
-                sizedBoxCustom(5),
+                          Container(
+                            child: PaymentHelper().bRow(
+                                'null',
+                                'Payment status',
+                                '${provider.subsData.paymentStatus}',
+                                lastBorder: false),
+                          ),
+                        ]),
+                  ),
 
-                CommonHelper().buttonPrimary(
-                    asProvider.getString('Renew Subscription'), () {
-                  provider.reniewSubscription(context);
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute<void>(
-                  //     builder: (BuildContext context) => PaymentChoosePage(
-                  //       reniewSubscription: true,
-                  //       price: provider.subsData.price,
-                  //     ),
-                  //   ),
-                  // );
-                }, isloading: provider.renewLoading)
-              ],
+                  sizedBoxCustom(5),
+
+                  CommonHelper().buttonPrimary(
+                      asProvider.getString('Renew Subscription'), () {
+                    provider.reniewSubscription(context);
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute<void>(
+                    //     builder: (BuildContext context) => PaymentChoosePage(
+                    //       reniewSubscription: true,
+                    //       price: provider.subsData.price,
+                    //     ),
+                    //   ),
+                    // );
+                  }, isloading: provider.renewLoading)
+                ],
+              ),
             ),
           ),
         ),
--- lib/view/subscription/subscription_history_page.dart
+++ lib/view/subscription/subscription_history_page.dart
@@ -36,81 +36,85 @@ class _SubscriptionHistoryPageState extends State<SubscriptionHistoryPage> {
       body: SingleChildScrollView(
         physics: physicsCommon,
         child: Consumer<SubscriptionService>(
-          builder: (context, provider, child) => provider.hasSubsHistory == true
-              ? provider.subsHistoryList.isNotEmpty
-                  ? Container(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: screenPadding, vertical: 10),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          for (int i = 0;
-                              i < provider.subsHistoryList.length;
-                              i++)
-                            Container(
-                              margin: const EdgeInsets.only(bottom: 16),
-                              width: double.infinity,
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 20, vertical: 16),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(9)),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          CommonHelper().titleCommon(
-                                              '${provider.subsHistoryList[i].type} ' +
+          builder: (context, provider, child) => Consumer<SubscriptionService>(
+            builder: (context, provider, child) =>
+                provider.hasSubsHistory == true
+                    ? provider.subsHistoryList.isNotEmpty
+                        ? Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: screenPadding, vertical: 10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                for (int i = 0;
+                                    i < provider.subsHistoryList.length;
+                                    i++)
+                                  Container(
+                                    margin: const EdgeInsets.only(bottom: 16),
+                                    width: double.infinity,
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 20, vertical: 16),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(9)),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                CommonHelper().titleCommon(
+                                                    '${provider.subsHistoryList[i].type} ' +
+                                                        asProvider.getString(
+                                                            'subscription'),
+                                                    fontsize: 15),
+                                                sizedBoxCustom(8),
+                                                Text(
                                                   asProvider.getString(
-                                                      'subscription'),
-                                              fontsize: 15),
-                                          sizedBoxCustom(8),
-                                          Text(
-                                            asProvider.getString("Start date") +
-                                                ": ${formatDate(provider.subsHistoryList[i].createdAt)}",
-                                            style: TextStyle(
-                                              color: cc.greyFour,
-                                              fontSize: 14,
-                                              height: 1.4,
-                                            ),
-                                          ),
-                                          sizedBoxCustom(6),
-                                          Text(
-                                            asProvider
-                                                    .getString("Expire date") +
-                                                ": ${formatDate(provider.subsHistoryList[i].expireDate)}",
-                                            style: TextStyle(
-                                              color: cc.greyFour,
-                                              fontSize: 14,
-                                              height: 1.4,
-                                            ),
-                                          ),
-                                        ]),
+                                                          "Start date") +
+                                                      ": ${formatDate(provider.subsHistoryList[i].createdAt)}",
+                                                  style: TextStyle(
+                                                    color: cc.greyFour,
+                                                    fontSize: 14,
+                                                    height: 1.4,
+                                                  ),
+                                                ),
+                                                sizedBoxCustom(6),
+                                                Text(
+                                                  asProvider.getString(
+                                                          "Expire date") +
+                                                      ": ${formatDate(provider.subsHistoryList[i].expireDate)}",
+                                                  style: TextStyle(
+                                                    color: cc.greyFour,
+                                                    fontSize: 14,
+                                                    height: 1.4,
+                                                  ),
+                                                ),
+                                              ]),
+                                        ),
+                                        // Icon(
+                                        //   Icons.arrow_forward_ios,
+                                        //   size: 16,
+                                        //   color: cc.greyFour,
+                                        // )
+                                      ],
+                                    ),
                                   ),
-                                  // Icon(
-                                  //   Icons.arrow_forward_ios,
-                                  //   size: 16,
-                                  //   color: cc.greyFour,
-                                  // )
-                                ],
-                              ),
+                              ],
                             ),
-                        ],
+                          )
+                        : Container(
+                            height: screenHeight(context) - 140,
+                            alignment: Alignment.center,
+                            child: OthersHelper().showLoading(cc.primaryColor),
+                          )
+                    : Container(
+                        height: screenHeight(context) - 120,
+                        alignment: Alignment.center,
+                        child: Text(asProvider.getString('No history found')),
                       ),
-                    )
-                  : Container(
-                      height: screenHeight(context) - 140,
-                      alignment: Alignment.center,
-                      child: OthersHelper().showLoading(cc.primaryColor),
-                    )
-              : Container(
-                  height: screenHeight(context) - 120,
-                  alignment: Alignment.center,
-                  child: Text(asProvider.getString('No history found')),
-                ),
+          ),
         ),
       ),
     );
